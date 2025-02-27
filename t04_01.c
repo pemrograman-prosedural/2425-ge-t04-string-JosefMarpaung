cce1de9f5ed4753b4802ddd96f0410032e7cd9b9
JosefMarpaung <josefchristian31@gmail.com>
2025-02-27 14:53:42 +0700

    pair 18

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,30 @@
-// NIM - Name
-// NIM - Name
+// 12S24010 - Artha Liebe Siregar
+// 12S24036 - Josef Christian Marpaung 
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  char s[61];
+  scanf("%60s", s);
+
+  int len = strlen(s);
+  for (int i = 0; i < len; i += 3) 
+  {
+    char temp[4];
+
+    strncpy(temp, s + i, 3);
+
+    temp[3] = '\0';
+
+    int ascii = atoi(temp);
+
+    printf("%c", (char)ascii);
+  }
+   
+   printf("\n");
+
 
   return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,20 @@
-// NIM - Name
-// NIM - Name
+// 12S24010 - Artha Liebe Siregar
+// 12S24036 - Josef Christian Marpaung
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  char huruf[21];
 
-  return 0;
+  fgets(huruf, sizeof(huruf), stdin);
+  
+  huruf[strcspn(huruf, "\n")] = '\0';
+  
+  for (int i = 0; i < strlen(huruf); i++) {
+      printf("%03d", huruf[i]);
+  }
+  
+  printf("013\n");
+return 0;
 }
